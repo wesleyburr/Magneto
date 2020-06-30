aafa581074a1de4fb4c20db7a850ad97aa06eaea
Ben Ott <benjaminott@trentu.ca>
2020-06-30 17:18:12 -0400

    impimented the python code for finding peaks

--- Scripts/findPeaks.py
+++ Scripts/findPeaks.py
@@ -1,2 +1,8 @@
-import numpy as np
-import peakutils.peak
+from scipy import signal
+
+def FindingPeaks(rowSums, minPeakHeight, distance):
+  
+  indexes = signal.find_peaks(rowSums, height=minPeakHeight, distance=distance)
+  return indexes
+  
+
