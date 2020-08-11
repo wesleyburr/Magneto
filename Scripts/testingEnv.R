7420895e5e51e741d8ace5306bc737420762203f
Ben Ott <benjaminott@trentu.ca>
2020-08-11 12:11:34 -0400

    added bottom envelope to these two files :

--- Scripts/testingEnv.R
+++ Scripts/testingEnv.R
@@ -45,71 +45,77 @@ ImageTesting <- read.csv("~/Magneto2020/DataCSV/TODOBatch5.csv", header = TRUE, 
 #   dev.off()
 # }
 
+topBound <- .top_env(rolledImage, max_roc = 25)
 
 
-max_roc <- 10
 
-min_white <- apply(rolledImage, MARGIN = 2, FUN = function(x) {
-  if (sum(x) == 0) {
-    white <- 0
-  }
-  else {
-    white <- min( which(x == 1) )
-  }
-  return(white)
-})
-#starting at the middle to be safe, will work backwords after
-foundNonZero <- FALSE
-for (i in (round(ncol(rolledImage))/2):ncol(rolledImage)) { # need for loop because need to be able to just back an index
-  x <- min_white[i]
-  #first column or no nonZero column found yet
-  if ( i == 1 || isFALSE(foundNonZero)) {
-    if (min_white[i] != 0) {
-      foundNonZero <- TRUE
+.bottom_env <- function(rolledImage, max_roc = 25, sepDist = 10){
+  max_white <- apply(rolledImage, MARGIN = 2, FUN = function(x) {
+    if (sum(x) == 0) {
+      white <- 0
     }
-  }
-  # a non zero column is found
-  else if (isTRUE(foundNonZero)) {
-    oneLess <- min_white[i - 1]
-    diff <- x - oneLess
-    if (diff >= max_roc) { # big change, could be a jump
-      if (diff > 0) { # new point is higher then old point
-        min_white[i] <- min_white[i - 1] + 2 # 2 added to be safe
-      }
-      if (diff < 0) { # new point is below old point, cold be a timing gap
-        min_white[i] <- min_white[i - 1] - 2 # 2 subtracted to be safe
+    else {
+      white <- max( which(x == 1) )
+    }
+    return(white)
+  })
+
+  #starting at the middle to be safe, will work backwards after
+  foundNonZero <- FALSE
+  for (i in (round(ncol(rolledImage))/2):ncol(rolledImage)) { # need for loop because need to be able to just back an index
+    x <- max_white[i]
+    #first column or no nonZero column found yet
+    if ( i == 1 || isFALSE(foundNonZero)) {
+      if (max_white[i] != 0) {
+        foundNonZero <- TRUE
       }
     }
+    # a non zero column is found
+    else if (isTRUE(foundNonZero)) {
+      oneLess <- max_white[i - 1]
+      diff <- x - oneLess
+      if (abs(diff) >= max_roc) { # big change, could be a jump
+        if (diff > 0) { # new point is higher then old point
+          max_white[i] <- max_white[i - 1]
+        }
+        if (diff < 0) { # new point is below old point, cold be a timing gap
+          max_white[i] <- max_white[i - 1]
+        }
+      }
+    }
   }
+
+  # The reverse, the first half of the image
+  foundNonZero <- FALSE
+  for (j in 1:(round(ncol(rolledImage))/2 )) { # need for loop because need to be able to just back an index
+    i <- (round(ncol(rolledImage))/2 + 1) - j
+    x <- max_white[i]
+    #first column or no nonZero column found yet
+    if ( j == 1 || isFALSE(foundNonZero)) {
+      if (max_white[i] != 0) {
+        foundNonZero <- TRUE
+      }
+    }
+    # a non zero column is found
+    else if (isTRUE(foundNonZero)) {
+      oneLess <- max_white[i + 1] # actually more because reverse indexing
+      diff <-  oneLess - x # remember that the picture is reversed aswell, 0 is the top..
+      if (abs(diff) >= max_roc) { # big change, could be a jump
+        #browser()
+        if (diff > 0) { # new point is higher then old point (looking at the image)
+          max_white[i] <- max_white[i + 1]
+        }
+        if (diff < 0) { # new point is below old point, could be a timing gap (lookug at the image)
+          max_white[i] <- max_white[i + 1]
+        }
+      }
+    }
+  }
+  correctedWhite <- nrow(rolledImage) - max_white + sepDist # because image is actually flipped with respect to the matrix
+  return(correctedWhite)
 }
 
-# The reverse, the first half of the image
-foundNonZero <- FALSE
-for (j in 1:(round(ncol(rolledImage))/2 )) { # need for loop because need to be able to just back an index
-  i <- (round(ncol(rolledImage))/2 + 1) - j
-  x <- min_white[i]
-  #first column or no nonZero column found yet
-  if ( j == 1 || isFALSE(foundNonZero)) {
-    if (min_white[i] != 0) {
-      foundNonZero <- TRUE
-    }
-  }
-  # a non zero column is found
-  else if (isTRUE(foundNonZero)) {
-    oneLess <- min_white[i + 1] # actually more because reverse indexing
-    diff <-  oneLess - x # remember that the picture is reversed aswell, 0 is the top..
-    if (diff >= max_roc) { # big change, could be a jump
-      #browser()
-      if (diff > 0) { # new point is higher then old point (looking at the image)
-        min_white[i] <- min_white[i + 1]
-      }
-      if (diff < 0) { # new point is below old point, cold be a timing gap (lookug at the image)
-        min_white[i] <- min_white[i + 1]
-      }
-    }
-  }
-}
-
-correctedWhite <- nrow(imageWithoutTopBottom) - min_white
-plot(imageWithoutTopBottom)
+plot(rolledImage)
 lines(y = correctedWhite, x = 1:ncol(imageWithoutTopBottom), col = "green")
+lines(y = topBound, x = 1:ncol(imageWithoutTopBottom), col = "green")
+
