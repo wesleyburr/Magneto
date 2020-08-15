415472104070edd5499bed6f2547614fdef14b37
Ben Ott <benjaminott@trentu.ca>
2020-08-15 19:46:18 -0400

    added implimentation of spliting traces, need to work more on the finding ends stuff, bug in .intersection_check

--- Scripts/testingEnv.R
+++ Scripts/testingEnv.R
@@ -3,32 +3,12 @@ library("zoo")
 library("rtiff")
 ImageTesting <- read.csv("~/Magneto2020/DataCSV/TODOBatch5.csv", header = TRUE, stringsAsFactors = FALSE)
 k <- 1
-set.seed(1)
+#set.seed(1)
 j <- sample(1:26000, size = 100, replace = FALSE)
-flag = FALSE
-#This doesnt work, scrap ...
-# .envelope_collapse_check <- function(upperTrace, lowerTrace, percentFromEdge = 2, maxCounts = 50){
-#   counter <- 0
-#   possibleCollapse <- vector()
-#   # checking for collapse in the envelope
-#   browser()
-#   for (i in 2:(min(c(length(upperTrace), length(lowerTrace))))) { # starts at 2 becuase 1st is accounted for with indexing
-#     if (counter == maxCounts) {
-#       possibleCollapse <- append(possibleCollapse, i)
-#       counter <- 0
-#     }
-#     else if (upperTrace[i] == upperTrace[i - 1] &
-#              lowerTrace[i] == lowerTrace[i - 1]) {
-#       counter <- counter + 1
-#     }
-#     else {
-#       counter <- 0
-#     }
-#   }
-#   return(possibleCollapse) # all the times this happens
-# }
 
-for (k in 1:5) {
+flag = FALSE
+
+for (k in 1:40) {
 
   i <- j[k]
   print(i)
@@ -39,13 +19,12 @@ for (k in 1:5) {
   imageMatrix <- import_process_image(imageName = imageName, file_loc = file_loc)
   imagecut <- .trim_top_bottom(imageMatrix, trimAmountTop = 100, trimAmountBottom = 50) #takes off the usual flair spots
   imageSides <- .get_trace_start_ends(imagecut, returnMat = FALSE, cutPercentage = 2) # two vertical lines
-  tripleBool <- .triple_check(imageMatrix = imageMatrix)
+  tripleBool <- .triple_check(imageMatrix = imageMatrix) #checking for triple trace images
   if (isTRUE(tripleBool)) {
     print("possible triple found!")
     next
   }
 
-  #imageMatrix <- edge_detection(imagecut, method = "Sobel")
   imageWithoutSides <- imageMatrix[, -c(0:imageSides$Start, imageSides$End:ncol(imageMatrix))] #takes away the sides found above
   # finds top horizontal line
   topcut <- .top_image_cut(imageMatrix = imageWithoutSides, percentFromEdge = 2, percentEdgeForLeft = 25)
@@ -63,47 +42,111 @@ for (k in 1:5) {
   #No warnings, then we can roll mean the image and work on the bounds
   else {#if (bottomcut != nrow(imageMatrix) & !inherits(topcut, "warning")) {
 
-   imageWithoutTopBottom <- imageMatrix[-c(0:topcut, bottomcut:nrow(image)), ]
-    vert <- t(imageWithoutTopBottom)
-    rolledImage <- t(rollmean(vert, k = 40, fill = "extend"))
-    rolledImage[which(rolledImage != 0)] <- 1
-
+    rolledImage <- mean_roll_image(imageMatrix, topcut, bottomcut)
     #the envelopes
-    topEnvelope <- .top_env(rolledImage, sepDist = 10, max_roc = 50, maxNoise = 100)
-    topEnvelopeScaled <- nrow(imageMatrix) - bottomcut + topEnvelope
-    bottomEnvelope <- .bottom_env(rolledImage, sepDist = 10, max_roc = 50, maxNoise = 100)
-    bottomEnvelopeScaled <- nrow(imageMatrix) - bottomcut + bottomEnvelope
-    topLowerEnv <- .top_lower_env(rolledImage, sepDist = 10, max_roc = 50, maxNoise = 100)
-    topLowerEnvScaled <- nrow(imageMatrix) - bottomcut + topLowerEnv
-    bottomUpperEnv <- .bottom_upper_env(rolledImage, sepDist = 10, max_roc = 50, maxNoise = 100)
-    bottomUpperEnvScaled <- nrow(imageMatrix) - bottomcut + bottomUpperEnv
+    #bottomEnvelope <- .bottom_env(rolledImage, sepDist = 10, max_roc = 50, maxNoise = 100)
+    #bottomUpperEnv <- .bottom_upper_env(rolledImage, sepDist = 10, max_roc = 50, maxNoise = 100)
+    #topLowerEnv <- .top_lower_env(rolledImage, sepDist = 10, max_roc = 50, maxNoise = 100)
 
-    browser()
-    possibleColTop <- .envelope_collapse_check(topEnvelope, topLowerEnv)
-    possibleColBottom <- .envelope_collapse_check(bottomUpperEnv, bottomEnvelope)
-    possibleColTopScaled <- nrow(imageMatrix) - bottomcut + possibleColTop
-    possibleColBottomScaled <- nrow(imageMatrix) - bottomcut + possibleColBottom
-    browser()
-    if (length(possibleColBottom) == 0) {
-      possibleColBottom <- 1
+    #topEnvelopeScaled <- nrow(imageMatrix) - bottomcut + topEnvelope
+    #bottomEnvelopeScaled <- nrow(imageMatrix) - bottomcut + bottomEnvelope
+    #topLowerEnvScaled <- nrow(imageMatrix) - bottomcut + topLowerEnv
+    #bottomUpperEnvScaled <- nrow(imageMatrix) - bottomcut + bottomUpperEnv
+
+    MatrixEnvelopes <- find_envelopes(rolledImage = rolledImage, imageMatrix = imageMatrix,
+                                    bottomcut = bottomcut, returnType = "MatrixScaled")
+    topEnvelopeMatrixScaled <- MatrixEnvelopes$TopEnvelope
+    topLowerEnvelopeMatrixScaled <- MatrixEnvelopes$TopLowerEnvelope
+    bottomUpperEnvelopeMatrixScaled <- MatrixEnvelopes$BottomUpperEnvelope
+    bottomEnvelopeMatrixScaled <- MatrixEnvelopes$BottomEnvelope
+
+    # topEnvelopeMatrixScaled <- bottomcut - topEnvelope
+    # topLowerEnvelopeMatrixScaled <- bottomcut - topLowerEnv
+    # bottomUpperEnvelopeMatrixScaled <- bottomcut - bottomUpperEnv
+    # bottomEnvelopeMatrixScaled <- bottomcut - bottomEnvelope
+
+
+    topTraceMatrix <- .isolating_trace(imageMatrix, topEnvelopeMatrixScaled,
+                                       topLowerEnvelopeMatrixScaled)
+    bottomTraceMatrix <- .isolating_trace(imageMatrix, bottomUpperEnvelopeMatrixScaled,
+                                          bottomEnvelopeMatrixScaled)
+
+    .envStartEnds <- function(traceMatrix, thresh = 200){
+      possibleStarts <- vector()
+      for (i in 1:ncol(traceMatrix)) {
+
+        if (sum(traceMatrix[,i]) == 0) {
+          possibleStarts <- append(possibleStarts, i)
+        }
+      }
+      possibleEnds <- sort(possibleStarts, decreasing = TRUE)
+      diffFoward <- diff(possibleStarts)
+      diffReverse <- diff(possibleEnds)
+      # getting the left side start
+      diffStarts <- which(diffFoward > thresh) # which gap could be the gap to the other side of the trace
+      if (length(diffStarts) == 0 || length(diffStarts) == Inf ) {
+        StartPoint <- 0
+      }
+      else {
+        StartPoint <- min(possibleStarts[diffStarts])
+
+      }
+      #going the other way to get the right side
+      diffEnds <- which(abs(diffReverse) > thresh) # which gap could be the gap to the other side of the trace
+      if (length(diffEnds) == 0 || length(diffEnds) == Inf ) {
+        EndPoint <- 0
+      }
+      else {
+        EndPoint <- min(possibleEnds[diffEnds])
+
+      }
+      if (EndPoint < StartPoint) {
+        StartPoint <- 0
+        EndPoint <- length(traceMatrix)
+      }
+      return(list(StartPoint = StartPoint, EndPoint = EndPoint))
     }
-    if (length(possibleColTop) == 0) {
-      possibleColTop <- 1
-    }
-     # Open a png file
+
+    TopStartsEnds <- .envStartEnds(topTraceMatrix)
+    BottomStartsEnds <- .envStartEnds(bottomTraceMatrix)
+
+    plotEnvelopes <- find_envelopes(rolledImage = rolledImage, imageMatrix = imageMatrix,
+                                    bottomcut = bottomcut, returnType = "PlottingScaled")
+
+    # Open a png file
     png(paste0("~/Magneto2020/plottingTesting/", imageName, ".png"))
     # 2. Create a plot
     suppressWarnings(plot(imageMatrix))
-    lines(topEnvelopeScaled, col = "green")
-    lines(bottomEnvelopeScaled, col = "yellow")
-    lines(topLowerEnvScaled, col = "green")
-    lines(bottomUpperEnvScaled, col = "yellow")
+    lines(plotEnvelopes$TopEnvelope, col = "green")
+    lines(plotEnvelopes$TopLowerEnvelope, col = "yellow")
+    lines(plotEnvelopes$BottomUpperEnvelope, col = "green")
+    lines(plotEnvelopes$BottomEnvelope, col = "yellow")
     abline(h = (nrow(imageMatrix) - topcut), col = "red")
     abline(h = (nrow(imageMatrix) - bottomcut), col = "red")
-    abline(h = possibleColTop, col = "green")
-    abline(h = possibleColBottom, col = "orange")
     # Close the pdf file
     dev.off()
+
+
+    png(paste0("~/Magneto2020/plottingTesting/", imageName, "-TopTrace" ,".png"))
+    # 2. Create a plot
+    suppressWarnings(plot(topTraceMatrix))
+    abline(v = c(TopStartsEnds$StartPoint, TopStartsEnds$EndPoint), col = "red")
+    lines(plotEnvelopes$TopEnvelope, col = "green")
+    lines(plotEnvelopes$TopLowerEnvelope, col = "yellow")
+    # Close the pdf file
+    dev.off()
+
+    png(paste0("~/Magneto2020/plottingTesting/", imageName, "-BottomTrace", ".png"))
+    # 2. Create a plot
+    suppressWarnings(plot(bottomTraceMatrix))
+    abline(v = c(BottomStartsEnds$StartPoint, BottomStartsEnds$EndPoint), col = "red")
+    lines(plotEnvelopes$BottomUpperEnvelope, col = "green")
+    lines(plotEnvelopes$BottomEnvelope, col = "yellow")
+    # Close the pdf file
+    dev.off()
+
+
+
 
     # if (imageSides$End >=  length(topLowerEnv)) {
     #   end <- length(topLowerEnv - 200) # 200 to keep away from the sides
@@ -111,12 +154,11 @@ for (k in 1:5) {
     # else {
     #   end <- imageSides$End
     # }
-    for (m in 300:(min(c(length(bottomUpperEnv), length(topLowerEnv))) - 300)) { #Checking for intersection between the two lines
-      if (topLowerEnv[m] <= bottomUpperEnv[m]) {
-        print(warning(paste0("There is an intersection at (", topLowerEnv[m], ", ", m, ")")))
-        print(imageName)
-        break
-      }
+
+    intersection <- tryCatch(.intersection_check(MatrixEnvelopes$TopEnvelope,
+                                                 MatrixEnvelopes$BottomUpperEnv, imageName), warning = function(w) w)
+    if (inherits(intersection, "warning")) {
+      print(intersection)
     }
   }
   if (isTRUE(flag)) { # plotting so I can see whats wrong
